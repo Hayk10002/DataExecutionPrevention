c87c8fc3f1465f9dd1070f27f691917afa9ada49
Hayk10002 <hayk.g.khachatryan@gmail.com>
2025-07-31 19:58:22 +0400

    Refactor `CMakeLists.txt` and `main.cpp` for improved executable options and function definition

--- main.cpp
+++ main.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 #include <algorithm>
-#include <memory>
+#include <cstring>
 
-int f()
+#if defined(_WIN32) || defined(_WIN64)
+  #include <process.h>  // for _exit on Windows
+#else
+  #include <unistd.h>   // for _exit on POSIX
+#endif
+
+void f()
 {
-    return 42;
+    std::cout << "42";
+    _exit(0);
 }
 
 int main()
 {
-    std::byte f_buffer[32];
-    std::copy((std::byte*)(&f), (std::byte*)(&f) + sizeof(f_buffer), f_buffer);
+    std::byte f_buffer[256];
+    std::memcpy((void*)f_buffer, (void*)&f, sizeof(f_buffer));
 
-    int (*f_in_stack)() = reinterpret_cast<int(*)()>(f_buffer);
+    auto f_in_stack = (void(*)())(f_buffer);
 
-    std::cout << f_in_stack() << std::endl;
-    return 0;
+    f_in_stack();
 }
